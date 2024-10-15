c4d9f9cddc267fc6cf85d279c19de1ab92bc237c
Ivan kern <vanatuz2@gmail.com>
2024-10-15 13:51:17 +0300

    feat: classifier models and admin

--- classifier/admin.py
+++ classifier/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Label, PlantInfo, PlantImage, Plant, Dataset
+
+admin.site.register(Label)
+
+
+class PromoImageInline(admin.StackedInline):
+    model = PlantImage
+    min_num = 1
+    max_num = 3
+
+
+class PlantInline(admin.StackedInline):
+    model = Plant
+    min_num = 2
+
+
+@admin.register(PlantInfo)
+class PlantInfoAdmin(admin.ModelAdmin):
+    inlines = (PromoImageInline, )
+
+    list_display = ("preview", "name", "full_name")
+
+    def preview(self, obj):
+        if obj.images.exists():
+            first_image = obj.images.all()[0]
+            try:
+                return mark_safe(f'<img src="{first_image.image.url}" height="80">')
+            except ValueError:
+                first_image.delete()
+                return "[no image]"
+        else:
+            return "[no image]"
+
+
+@admin.register(Dataset)
+class DatasetAdmin(admin.ModelAdmin):
+    inlines = (PlantInline, )
+    list_display = ("name", 'get_num_classes', "is_active",)
+
+    def get_num_classes(self, obj):
+        return len(obj.plants.all())
+
+    get_num_classes.short_description = 'Number of classes'
--- classifier/models.py
+++ classifier/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.db.models import Q
 
-# Create your models here.
+
+class Label(models.Model):
+    value = models.SmallIntegerField(unique=True)
+
+    def __str__(self):
+        return str(self.value)
+
+
+class PlantInfo(models.Model):
+    name = models.CharField(max_length=70)
+    full_name = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class PlantImage(models.Model):
+    promo = models.ForeignKey(PlantInfo, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to="media/classifier/")
+
+    def __str__(self):
+        return self.promo.name
+
+
+class Dataset(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.TextField(null=True, blank=True)
+    is_active = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if kwargs.get("is_active"):
+            Dataset.objects.all().update({"is_active": False})
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        super().clean()
+        if self.is_active:
+            Dataset.objects.filter(~Q(id=self.id)).update(is_active=False)
+
+
+class Plant(models.Model):
+    label = models.ForeignKey(Label, on_delete=models.DO_NOTHING)
+    info = models.ForeignKey(PlantInfo, on_delete=models.DO_NOTHING)
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name="plants", null=True, blank=True)
+
+    class Meta:
+        unique_together = (("label", "dataset"), )
